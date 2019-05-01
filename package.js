e7ea79792154d3438b29d3d5e1a330830e86c5d7
mifwek <44892124+mifwek@users.noreply.github.com>
2019-05-01 17:34:28 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "folder-baru",
+  "version": "1.0.0",
+  "description": "taek",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "mifta",
+  "license": "ISC",
+  "dependencies": {
+    "discord.js": "^11.4.2",
+    "embed-video": "^2.0.4",
+    "ffmpeg-binaries": "^4.0.0",
+    "figlet": "^1.2.1",
+    "opusscript": "0.0.6",
+    "simple-youtube-api": "^5.1.1",
+    "ytdl-core": "^0.28.0"
+  }
+}
